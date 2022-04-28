2be073dcd4256e56e874ca0f34b5ad00b87945eb
zkato <Zkatona51@yahoo.com>
2022-04-28 20:11:55 +0300

    Ball animations for penalty game

---
+++ ClientApp/src/components-styles/Penalty.style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Background = styled.div`
+    width: 100%;
+    padding: 78px 0px;
+    background-color: #191919;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    padding-top: 160px;
+`
--- ClientApp/src/components/Penalty.js
+++ ClientApp/src/components/Penalty.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Goalkeeper } from '../animated-resources/Goalkeeper';
+import { Background } from '../components-styles/Penalty.style';
+import '../animated-resources/animations.css'
 
 export class Penalty extends Component {
     static displayName = Penalty.name;
     render(){
         return (
-            <div>Penalty</div>
+            <Background style={{backgroundImage: `url(https://images.unsplash.com/photo-1570277756464-146a053f8a0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)`}}>
+                <Goalkeeper/>
+            </Background>
         );
     }
 }
